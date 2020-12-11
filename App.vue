a2bb547fbb2f12edfdfeea1bcce4804e769afa31
Hritesh <61223247+hritesh93@users.noreply.github.com>
2020-12-11 11:02:12 +0530

    reactive state, store class

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="post in store.state.posts">
+    {{ post.title }}
+  </div>
+</template>
+
+<script>
+import { store } from './store'
+  export default {
+    setup() {
+      return {
+        store
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ index.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import Microblog from './App.vue'
+
+createApp(Microblog).mount('#app')
