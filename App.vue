a0288cdbeb9d672ec3659b2f0b84efcca31b2780
Hritesh <61223247+hritesh93@users.noreply.github.com>
2020-12-11 19:10:34 +0530

    hashtag, emit, cards

--- App.vue
+++ App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div v-for="post in store.state.posts">
+  <card 
+    v-for="post in store.state.posts"
+  >
+  <template v-slot:title>
     {{ post.title }}
-  </div>
+  </template>
+
+  <template v-slot:content>
+    {{ post.content }}
+  </template>
+
+  <template v-slot:description>
+    <Controls :post="post" />
+  </template>
+  </card>
+  {{ currentHashtag }}
 </template>
 
 <script>
 import { store } from './store'
+import Card from './Card.vue'
+import Controls from './Controls.vue'
+import { ref } from 'vue'
+
   export default {
+    components: {
+      Card,
+      Controls
+    },
     setup() {
+      const currentHashtag = ref()
+      const setHashtag = (tag) => {
+        currentHashtag.value = tag
+      }
       return {
-        store
+        store,
+        setHashtag, 
+        currentHashtag
       }
     }
   }
---
+++ Controls.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button>
+      {{ post.likes }}
+    </button>
+    <Hashtag 
+      v-for="tag in post.hashtags" 
+      :hashtag="tag"
+      @setHashtag="setHashtag"
+    />
+  </div>
+</template>
+
+<script>
+import Hashtag from './Hashtag.vue'
+
+export default {
+  components: {
+    Hashtag
+  },
+
+  props: {
+    post: {
+      type: Object
+    }
+  },
+
+  setup(props, ctx) {
+    const setHashtag =(tag) => {
+      console.log(tag);
+      ctx.emit('setHashtag', props.hashtag)
+    }
+    return {
+      setHashtag
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
